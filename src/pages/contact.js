008f9a53f6f2d4645e9e532e5acf5d4447effd74
Peter Bellaby <bellaby1993@hotmail.com>
2022-05-18 18:50:11 +0100

    Updated with client feedback

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,9 +5,8 @@ import { Layout } from "../components/layout/Layout"
 const AboutPage = () => {
   return (
     <Layout>
-      <h1 className="text-6xl text-center font-medium p-2">About Yi</h1>
-      <p className="text-2xl text-center px-6 py-2">Get a digital drawing of yourself in my style!</p>
-      <p className="text-2xl text-center underline px-6 py-2">Contact Me:</p>
+      <h1 className="text-6xl text-center font-medium p-2">Contact Me</h1>
+      <p className="text-3xl text-center underline px-6 py-2">Available at:</p>
       <div className="w-3/5 mx-auto bg-bgdark rounded-3xl">
         <ul className="list-none text-center">
           <li><p className="text-2xl px-6 py-2">E-mail  - <a href="mailto:carina0425@gmail.com" className="font-bold">Carina0425@gmail.com</a></p></li>
